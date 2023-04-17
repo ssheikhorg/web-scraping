5b916268c20c8d6742a1754f776e280d0037a60a
Shapon Sheikh <shsheikhbd@gmail.com>
2023-04-17 19:39:20 +0600

    finalized crawling data as much as possible and export it to csv and json

--- crawl/spiders/adidas.py
+++ crawl/spiders/adidas.py
@@ -1,20 +1,14 @@
 from typing import Any
 
-import scrapy
 from scrapy.http import HtmlResponse
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
 
 
 class AdidasSpider(CrawlSpider):
     name = "adidas"
     allowed_domains = ["shop.adidas.jp"]
     start_urls = ["https://shop.adidas.jp/"]
-
-    # https://shop.adidas.jp/men/
-    # https://shop.adidas.jp/item/?cat2Id=marimekko23ss&gender=mens&order=1
-    # https://shop.adidas.jp/products/HR3028/
 
     # """parse data from product page while mounting from men to item"""
     rules = (
@@ -25,7 +19,6 @@ class AdidasSpider(CrawlSpider):
 
     def parse_product(self, response: HtmlResponse) -> dict[str, Any]:
         """parse product page"""
-        # parse only 5 items
         if self.crawler.stats.get_value("downloader/response_count") > 5:
             self.crawler.engine.close_spider(self, "crawled enough items")
 
@@ -33,8 +26,12 @@ class AdidasSpider(CrawlSpider):
             "breadcrumb_category": response.css(".breadcrumbListItemLink::text").getall(),
             "category": f'{response.css(".test-genderName::text").get()} {response.css(".test-categoryName::text").get()}',
             "product_name": response.css('.test-itemTitle::text').get(),
-            "price": response.css(".test-price-value::text").get(),
-            "image_url": response.css(".test-slider-list img::attr(src)").getall(),
-            "coordinated_product_name": response.css(".test-coordinated-itemTitle::text").getall(),
-            "coordinated_price": response.css(".test-price-value::text").get(),
+            "pricing": response.css(".test-price-value::text").get(),
+            "image_urls": ", ".join(
+                ["https://shop.adidas.jp" + url for url in response.css('.main_image::attr(src)').getall()]),
+            "descriptions_title": response.css('.test-commentItem-subheading::text').get(),
+            "descriptions": response.css('.test-commentItem-mainText::text').get(),
+            "description_itemized": response.css('.test-feature::text').getall(),
+            "sizes": response.css("ul.sizeSelectorList > li > button::text").getall(),
+            "tags": response.css(".itemTagsPosition > div > div > a::text").getall()
         }
